66055d9deb178be04f2f5fa08fe00abfb7a4edcb
NamanGoyal07 <naman18@iitg.ac.in>
2021-03-02 23:55:36 +0530

    Sidebar Completed

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,14 +4,28 @@ import SidebarRow from './SidebarRow.js';
 import HomeIcon from "@material-ui/icons/Home";
 import WhatshotIcon from "@material-ui/icons/Whatshot";
 import SubsciptionsIcon from "@material-ui/icons/Subscriptions";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import HistoryIcon from "@material-ui/icons/History";
+import OndemandVideoIcon from "@material-ui/icons/OndemandVideo";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbUpAltOutlined";
+import ExpandMoreOutlinedIcon from "@material-ui/icons/ExpandMoreOutlined";
 
 function Sidebar() {
     return (
         <div className="sidebar">
             {/* <h2>This is the sidebar</h2> */}
-            <SidebarRow Icon = {HomeIcon} title = "Home"/>
+            <SidebarRow selected Icon = {HomeIcon} title = "Home"/>
             <SidebarRow Icon = {WhatshotIcon} title = "Trending"/>
             <SidebarRow Icon = {SubsciptionsIcon} title = "Subsciption"/>
+            <hr/>
+            <SidebarRow Icon = {VideoLibraryIcon} title = "Library" />
+            <SidebarRow Icon = {HistoryIcon} title = "History" />
+            <SidebarRow Icon = {OndemandVideoIcon} title = "Your videos" />
+            <SidebarRow Icon = {WatchLaterIcon} title = "Watch Later" />
+            <SidebarRow Icon = {ThumbUpAltOutlinedIcon} title = "Liked Videos" />
+            <SidebarRow Icon = {ExpandMoreOutlinedIcon} title = "Show more" />
+            <hr />
         </div>
     )
 }
