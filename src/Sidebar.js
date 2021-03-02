9d2d6c3ea9681d333291ce9208f40dfef438a703
NamanGoyal07 <naman18@iitg.ac.in>
2021-03-02 14:10:06 +0530

    Sidebar main icons added

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Sidebar.css';
+import SidebarRow from './SidebarRow.js';
+import HomeIcon from "@material-ui/icons/Home";
+import WhatshotIcon from "@material-ui/icons/Whatshot";
+import SubsciptionsIcon from "@material-ui/icons/Subscriptions";
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h2>This is the sidebar</h2>
+            {/* <h2>This is the sidebar</h2> */}
+            <SidebarRow Icon = {HomeIcon} title = "Home"/>
+            <SidebarRow Icon = {WhatshotIcon} title = "Trending"/>
+            <SidebarRow Icon = {SubsciptionsIcon} title = "Subsciption"/>
         </div>
     )
 }
