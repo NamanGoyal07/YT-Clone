9f24b09fa387616251fa3e120e01bfe6365c358e
NamanGoyal07 <naman18@iitg.ac.in>
2021-03-03 13:32:48 +0530

    Videos added

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -27,6 +27,79 @@ function RecommendedVideos() {
                     channelimage="https://yt3.ggpht.com/yti/ANoDKi4H2giODBfCok8gc4zZcy9q0DjdckXPE21-HIBDEA=s88-c-k-c0x00ffffff-no-rj-mo"
                     image = "https://i9.ytimg.com/vi/9z7LIvwwOrQ/mqdefault.jpg?time=1614754500000&sqp=CMTl_IEG&rs=AOn4CLB4wmLFuNdQvPJP0aCeV04_shp9PQ"
                 />
+
+<VideoCard 
+                    title="Real Time ChatCord"
+                    views="2.3M views"
+                    timestamp="3 days ago"
+                    channel="Naman Goyal"
+                    channelimage="https://yt3.ggpht.com/yti/ANoDKi4H2giODBfCok8gc4zZcy9q0DjdckXPE21-HIBDEA=s88-c-k-c0x00ffffff-no-rj-mo"
+                    image = "https://i9.ytimg.com/vi/HCZkwoGw38M/mq2.jpg?sqp=CMTl_IEG&rs=AOn4CLCFhBvS56LVYklHBDI8Nqim1yREVw"
+                />
+
+                <VideoCard 
+                    title="Virtual Memory Concept"
+                    views="4.5M views"
+                    timestamp="5 days ago"
+                    channel="Naman Goyal"
+                    channelimage="https://yt3.ggpht.com/yti/ANoDKi4H2giODBfCok8gc4zZcy9q0DjdckXPE21-HIBDEA=s88-c-k-c0x00ffffff-no-rj-mo"
+                    image = "https://i9.ytimg.com/vi/9z7LIvwwOrQ/mqdefault.jpg?time=1614754500000&sqp=CMTl_IEG&rs=AOn4CLB4wmLFuNdQvPJP0aCeV04_shp9PQ"
+                />
+
+<VideoCard 
+                    title="Real Time ChatCord"
+                    views="2.3M views"
+                    timestamp="3 days ago"
+                    channel="Naman Goyal"
+                    channelimage="https://yt3.ggpht.com/yti/ANoDKi4H2giODBfCok8gc4zZcy9q0DjdckXPE21-HIBDEA=s88-c-k-c0x00ffffff-no-rj-mo"
+                    image = "https://i9.ytimg.com/vi/HCZkwoGw38M/mq2.jpg?sqp=CMTl_IEG&rs=AOn4CLCFhBvS56LVYklHBDI8Nqim1yREVw"
+                />
+
+                <VideoCard 
+                    title="Virtual Memory Concept"
+                    views="4.5M views"
+                    timestamp="5 days ago"
+                    channel="Naman Goyal"
+                    channelimage="https://yt3.ggpht.com/yti/ANoDKi4H2giODBfCok8gc4zZcy9q0DjdckXPE21-HIBDEA=s88-c-k-c0x00ffffff-no-rj-mo"
+                    image = "https://i9.ytimg.com/vi/9z7LIvwwOrQ/mqdefault.jpg?time=1614754500000&sqp=CMTl_IEG&rs=AOn4CLB4wmLFuNdQvPJP0aCeV04_shp9PQ"
+                />
+
+<VideoCard 
+                    title="Real Time ChatCord"
+                    views="2.3M views"
+                    timestamp="3 days ago"
+                    channel="Naman Goyal"
+                    channelimage="https://yt3.ggpht.com/yti/ANoDKi4H2giODBfCok8gc4zZcy9q0DjdckXPE21-HIBDEA=s88-c-k-c0x00ffffff-no-rj-mo"
+                    image = "https://i9.ytimg.com/vi/HCZkwoGw38M/mq2.jpg?sqp=CMTl_IEG&rs=AOn4CLCFhBvS56LVYklHBDI8Nqim1yREVw"
+                />
+
+                <VideoCard 
+                    title="Virtual Memory Concept"
+                    views="4.5M views"
+                    timestamp="5 days ago"
+                    channel="Naman Goyal"
+                    channelimage="https://yt3.ggpht.com/yti/ANoDKi4H2giODBfCok8gc4zZcy9q0DjdckXPE21-HIBDEA=s88-c-k-c0x00ffffff-no-rj-mo"
+                    image = "https://i9.ytimg.com/vi/9z7LIvwwOrQ/mqdefault.jpg?time=1614754500000&sqp=CMTl_IEG&rs=AOn4CLB4wmLFuNdQvPJP0aCeV04_shp9PQ"
+                />
+
+<VideoCard 
+                    title="Real Time ChatCord"
+                    views="2.3M views"
+                    timestamp="3 days ago"
+                    channel="Naman Goyal"
+                    channelimage="https://yt3.ggpht.com/yti/ANoDKi4H2giODBfCok8gc4zZcy9q0DjdckXPE21-HIBDEA=s88-c-k-c0x00ffffff-no-rj-mo"
+                    image = "https://i9.ytimg.com/vi/HCZkwoGw38M/mq2.jpg?sqp=CMTl_IEG&rs=AOn4CLCFhBvS56LVYklHBDI8Nqim1yREVw"
+                />
+
+                <VideoCard 
+                    title="Virtual Memory Concept"
+                    views="4.5M views"
+                    timestamp="5 days ago"
+                    channel="Naman Goyal"
+                    channelimage="https://yt3.ggpht.com/yti/ANoDKi4H2giODBfCok8gc4zZcy9q0DjdckXPE21-HIBDEA=s88-c-k-c0x00ffffff-no-rj-mo"
+                    image = "https://i9.ytimg.com/vi/9z7LIvwwOrQ/mqdefault.jpg?time=1614754500000&sqp=CMTl_IEG&rs=AOn4CLB4wmLFuNdQvPJP0aCeV04_shp9PQ"
+                />
+
             </div>
 
         </div>
