0e6de0a9826bd32bbf80ccf694ac8cb164cb6bd7
NamanGoyal07 <naman18@iitg.ac.in>
2021-03-04 14:34:54 +0530

    Working on Channel desp

--- src/ChannelRow.js
+++ src/ChannelRow.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import Avatar from '@material-ui/core/Avatar';
+import './ChannelRow.css';
 
-function ChannelRow() {
-    return (
-        <div>
+function ChannelRow({ image, channel, verified, subs, noOfVideo, description }) {
+    return ( 
+        <div className = "channelRow" >
+        <Avatar className = "channelRow__logo"
+        alt = { channel }
+        src = { image }
+        /> 
+        <div className = "channelRow__text" >
             
-
+        </div>
         </div>
     )
 }
