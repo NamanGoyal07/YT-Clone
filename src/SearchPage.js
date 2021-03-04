630b232d97d33640a81d92d8b075c66cebe45fc1
NamanGoyal07 <naman18@iitg.ac.in>
2021-03-04 13:55:54 +0530

    Search on

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './SearchPage.css';
+import TuneOutlinedIcon from "@material-ui/icons/TuneOutlined";
 
 function SearchPage() {
     return (
         <div className="searchPage">
-            
+            <div className="searchPage__filter">
+            <TuneOutlinedIcon />
+            <h2>FILTER</h2>
+            </div>
         </div>
     )
 }
